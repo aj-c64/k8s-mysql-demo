b16fb1695929b21abccf397c4c3c696a814fd328
Megaton-216 <albertjcabrera@gmail.com>
2025-12-03 14:03:46 -0500

    Initial commit

---
+++ src/main/java/com/example/k8s_mysql_demo/K8sMysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8s_mysql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sMysqlDemoApplication.class, args);
+	}
+
+}
